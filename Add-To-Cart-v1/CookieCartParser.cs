7621837303a99f49811f3b72d60df8112a5df039
Farrah Marie Chavez <pangcaberte@gmail.com>
2019-05-27 19:13:36 +1200

    Modified the comments

--- Add-To-Cart-v1/CookieCartParser.cs
+++ Add-To-Cart-v1/CookieCartParser.cs
@@ -18,7 +18,7 @@ namespace Add_To_Cart_v1
         /// Parses the cookie cart into a Dictionary, making it more accessible
         /// </summary>
         /// <param name="strCookie">cookie string taken from the HTTPCookie with the pattern => id=quantity,id=quantity,..</param>
-        /// <returns>Dictionary</returns>
+        /// <returns>Dictionary equivalent of strCookie</returns>
         public Dictionary<string, string> ToDictionary(string strCookie)
         {
             dictCookie = new Dictionary<string, string>();
@@ -31,9 +31,9 @@ namespace Add_To_Cart_v1
         /// <summary>
         /// Adds new item to the string and returns the newly created string cookie
         /// </summary>
-        /// <param name="newItemID">the item to be added to the cookie</param>
-        /// <param name="strCookie">cookie string taken from the HTTPCookie</param>
-        /// <returns>new cookie string after addition</returns>
+        /// <param name="newItemID">The item to be added to the cookie</param>
+        /// <param name="strCookie">Cookie string taken from the HTTPCookie</param>
+        /// <returns>New cookie string after addition</returns>
         public string Add(string newItemID, string strCookie)
         {
             dictCookie = ToDictionary(strCookie);
@@ -55,9 +55,9 @@ namespace Add_To_Cart_v1
         /// <summary>
         /// Removes the item from the cookie
         /// </summary>
-        /// <param name="itemIDToRemove"></param>
-        /// <param name="strCookie"></param>
-        /// <returns>new cookie string after removal</returns>
+        /// <param name="itemIDToRemove">The item to be removed</param>
+        /// <param name="strCookie">Cookie string taken from the HTTPCookie</param>
+        /// <returns>New cookie string after removal</returns>
         public string Remove(string itemIDToRemove, string strCookie)
         {
             dictCookie = ToDictionary(strCookie);
@@ -70,10 +70,10 @@ namespace Add_To_Cart_v1
         /// <summary>
         /// Updates the item data in the cookie string
         /// </summary>
-        /// <param name="itemIDToUpdate">ID of item to update</param>
-        /// <param name="newData">new quantity of the item</param>
-        /// <param name="strCookie">cookie string</param>
-        /// <returns>new cookie string after addition</returns>
+        /// <param name="itemIDToUpdate">The item to be updated</param>
+        /// <param name="newData">New quantity of the item</param>
+        /// <param name="strCookie">Cookie string taken from the HTTPCookie</param>
+        /// <returns>New cookie string after update</returns>
         public string Update(string itemIDToUpdate, string newData, string strCookie)
         {
             dictCookie = ToDictionary(strCookie);
@@ -89,8 +89,8 @@ namespace Add_To_Cart_v1
         /// <summary>
         /// Converts the Dictionary cookie into a string
         /// </summary>
-        /// <param name="dictCookie">the dictionary containing cookie data</param>
-        /// <returns>string equivalent of the cookie</returns>
+        /// <param name="dictCookie">The dictionary containing the data from cookie</param>
+        /// <returns>String equivalent of the cookie</returns>
         public string Stringify(Dictionary<string, string> dictCookie)
         {
             string strCookie = string.Join(",", dictCookie.Select(x => x.Key + "=" + x.Value).ToArray());
@@ -103,8 +103,8 @@ namespace Add_To_Cart_v1
         /// <summary>
         /// Gets the number of items in the cookie
         /// </summary>
-        /// <param name="strCookie">cookie string</param>
-        /// <returns>number of items</returns>
+        /// <param name="strCookie">Cookie string taken from the HTTPCookie</param>
+        /// <returns>Number of items</returns>
         public int GetNumberOfItems(string strCookie)
         {
             int count = 0;
